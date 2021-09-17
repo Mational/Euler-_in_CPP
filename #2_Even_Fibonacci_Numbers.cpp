b76ea1ce4e11b65f1f6d07f5009f7dffa0b26998
Grzegorz Malinowski <grzegorzmal40@gmail.com>
2021-09-17 12:45:49 +0200

    Poprawa problemu #2 by działał również na Hackerranku

--- #2_Even_Fibonacci_Numbers.cpp
+++ #2_Even_Fibonacci_Numbers.cpp
@@ -4,17 +4,22 @@ using namespace std;
 
 int main()
 {
-    int a=1;
-    int b=1;
-    int c,s=0;
-    while( b<=4000000)
+    int t;
+    cin >> t;
+    for(int a0=0;a0<t;a0++)
     {
-        if( b%2==0 )
-            s+=b;
-        c=b;
-        b+=a;
-        a=c;
+        long limit;
+        cin >> limit;
+        long a=1, b=1, c, s=0;
+        while(b<=limit)
+        {
+            if( b%2==0 )
+                s+=b;
+            c=b;
+            b+=a;
+            a=c;
+        }
+        cout << s << endl;
     }
-    cout << "Wynik: " << s << endl;
     return 0;
 }
