e0dbae950fe4446cb75eb2a797a751439ea04924
Mational <grzegorzmal40@gmail.com>
2022-09-21 17:34:26 +0200

    Update Euler010 - Działa także na HackerRank

--- #010_Summation_of_primes.cpp
+++ #010_Summation_of_primes.cpp
@@ -1,27 +1,51 @@
 #include <iostream>
+#include <cmath>
+#include <vector>
 
 using namespace std;
 
-bool czy_pierwsza( int dane )
+bool czy_pierwsza(int dane)
 {
-    for(int j=2;j*j<=dane;j++)
-    {
+    int tmp = sqrt(dane);
+    for(int j=2;j<=tmp;j++)
         if(dane%j==0)
             return false;
-    }
     return true;
+}
+
+long long sum(vector<int>&v, int l)
+{
+    long long s=0;
+    int i=0;
+    while(v[i]<=l && i<v.size())
+    {
+        s+=v[i];
+        i++;
+    }
+    return s;
 }
 
 int main()
 {
-    long long suma=0;
-    for( int i=2; i<2000000; i++ )
+    vector<int> vec;
+    vec.push_back(2);
+
+    int t;
+    cin >> t;
+    for(int a0=0;a0<t;a0++)
     {
-        if( czy_pierwsza(i)==true )
-        {
-            suma+=i;
-        }
+        int limit;
+        cin >> limit;
+
+        long long suma = sum(vec,limit);
+        for(int i=vec[vec.size()-1]+1; i<=limit; i++)
+            if(czy_pierwsza(i))
+            {
+                vec.push_back(i);
+                suma+=i;
+            }
+
+        cout << suma << endl;
     }
-    cout << suma << endl;
     return 0;
 }
