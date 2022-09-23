d20489e97ca25de090a0409a9feb4fc2c79ab64c
Mational <grzegorzmal40@gmail.com>
2022-09-23 17:19:31 +0200

    Update Euler011 - usunięcie funkcji czytającej z pliku

--- #011_Largest_product_in_a_grid.cpp
+++ #011_Largest_product_in_a_grid.cpp
@@ -4,26 +4,6 @@
 
 using namespace std;
 
-void czytaj(int t[20][20],string sciezka)
-{
-    fstream plik;
-    plik.open(sciezka,ios::in);
-    while(!plik.eof())
-    {
-        int liczba;
-        for(int i=0;i<20;i++)
-        {
-            for(int j=0;j<20;j++)
-            {
-                plik >> liczba;
-                t[i][j]=liczba;
-            }
-        }
-    }
-    plik.close();
-    return;
-}
-
 void poziomo(int t[20][20],long long&maks)
 {
     for(int i=0;i<20;i++)
