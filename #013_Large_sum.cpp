cb7afac0a11866268ecf1c7bc2410ffad60ac533
Mational <grzegorzmal40@gmail.com>
2022-09-23 19:10:53 +0200

    Update Euler013 - działa także na HackerRanku

--- #013_Large_sum.cpp
+++ #013_Large_sum.cpp
@@ -1,47 +1,31 @@
 #include <iostream>
-#include <fstream>
 
 using namespace std;
-
-void read(string path,string num[])
-{
-    fstream file;
-    file.open(path,ios::in);
-    for(int i=0;i<100;i++)
-    {
-        file >> num[i];
-    }
-    file.close();
-    return;
-}
 
 string add(string r,string n)
 {
     if(r.size()<n.size())
-    {
         swap(r,n);
-    }
+
     int r_pos=r.size()-1;
     int n_pos=n.size()-1;
     int tmp=0;
+
     while(r_pos>=0)
     {
         int r_num=r[r_pos]-48;
         int n_num;
-        if(n_pos>=0)
-        {
-            n_num=n[n_pos]-48;
-        }
-        else
-        {
-            n_num=0;
-        }
+
+        if(n_pos>=0)     n_num=n[n_pos]-48;
+        else            n_num=0;
+
         int outcome=r_num+n_num+tmp;
         r[r_pos]=(outcome%10)+48;
         tmp=outcome/10;
         n_pos--;
         r_pos--;
     }
+
     if(tmp>0)
     {
         string stmp=to_string(tmp);
@@ -50,11 +34,11 @@ string add(string r,string n)
     return r;
 }
 
-void calculations(string num[])
+void calculations(string num[], int t)
 {
     string result=num[0];
     string sres;
-    for(int i=1;i<100;i++)
+    for(int i=1;i<t;i++)
     {
         sres=add(result,num[i]);
         result=sres;
@@ -66,8 +50,12 @@ void calculations(string num[])
 
 int main()
 {
-    string numbers[100];
-    read("#13_numbers.txt",numbers);
-    calculations(numbers);
+    int t;
+    cin >> t;
+    string numbers[t];
+    for(int a0=0; a0<t;a0++)
+        cin >> numbers[a0];
+
+    calculations(numbers, t);
     return 0;
 }
