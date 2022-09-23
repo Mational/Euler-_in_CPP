e381d23efd0040ad9efdbf2a263504a762018956
Mational <grzegorzmal40@gmail.com>
2022-09-23 18:58:59 +0200

    Update Euler012 - działa również na HackerRanku

--- #012_Highly_divisible_triangular_number.cpp
+++ #012_Highly_divisible_triangular_number.cpp
@@ -1,35 +1,53 @@
 #include <iostream>
+#include <cmath>
+#include <vector>
 
 using namespace std;
 
-int dzielniki(int ii)
+int dzielniki(int n)
 {
     int dziel=0;
-    for(int j=1;j*j<=ii;j++)
-    {
-        if(ii%j==0)
+    int sn = sqrt(n);
+    for(int j=1;j<=sn;j++)
+        if(n%j==0)
         {
             dziel++;
-            if(ii/j!=j && ii/j!=1)
+            if(n/j!=j && n/j!=1)
                 dziel++;
         }
-    }
     return dziel;
+}
+
+long long program(int limit, vector<int>&v_dziel, vector<long long>&v_num)
+{
+    for(int i=0;i<v_dziel.size();i++)
+        if(v_dziel[i]>limit)   return v_num[i];
+
+    int i=v_num.size() + 1;
+    long long sum = v_num[ v_num.size()-1] + i;
+
+    do
+    {
+        v_num.push_back(sum);
+        v_dziel.push_back(dzielniki(sum));
+        sum += ++i;
+    }while(v_dziel[v_dziel.size()-1]<=limit);
+
+    return v_num[v_num.size()-1];
 }
 
 int main()
 {
-    long long suma=0;
-    int max_dziel=0;
-    int i=1;
-    while(max_dziel<500)
+    int t;
+    cin >> t;
+    vector<int> v_dziel {0, 2, 4};
+    vector<long long> v_num {1, 3, 6};
+
+    for(int a0=0;a0<t;a0++)
     {
-        suma+=i;
-        int l_dzielnikow=dzielniki(suma);
-        if(l_dzielnikow>max_dziel)
-            max_dziel=l_dzielnikow;
-        i++;
+        int number;
+        cin >> number;
+        cout << program(number, v_dziel, v_num) << endl;
     }
-    cout << suma << endl;
     return 0;
 }
