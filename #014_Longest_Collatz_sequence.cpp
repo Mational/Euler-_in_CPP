b2e3e94c819b7f2ae1a59476cde9a1ca3bbaeeaa
Mational <grzegorzmal40@gmail.com>
2022-09-24 21:37:02 +0200

    Update Euler014 - HackerRank update
    All tests except 9,10, 11, 12 are passed.

--- #014_Longest_Collatz_sequence.cpp
+++ #014_Longest_Collatz_sequence.cpp
@@ -1,39 +1,54 @@
 #include <iostream>
+#include <map>
+#include <vector>
 
 using namespace std;
 
+map<long long, int> collatz_len;
+
 int collatz(long long liczba)
 {
-    int lancuch = 0;
-    while(liczba != 1)
+    if(liczba == 1) return 0;
+
+    if(collatz_len[liczba] == 0)
+        if(liczba%2 == 0)   collatz_len[liczba] = collatz(liczba/2) + 1;
+        else                collatz_len[liczba] = collatz(liczba*3+1) + 1;
+
+    return collatz_len[liczba];
+}
+
+int program(int length, vector<long long>&v)
+{
+    if(v.size() > length)   return v[length];
+    int max_liczba = v[v.size()-1];
+    int maks_lancuch = collatz_len[max_liczba];
+
+    int dl;
+    for(int i = v.size();i<=length;i++)
     {
-        lancuch++;
-        liczba = (liczba%2==0)?liczba/2:liczba*3+1;
+        dl=collatz(i);
+        if(dl>=maks_lancuch)
+        {
+            maks_lancuch=dl;
+            max_liczba=i;
+        }
+        v.push_back(max_liczba);
     }
-    return lancuch;
+    return max_liczba;
 }
 
 int main()
 {
     int t;
     cin >> t;
+    collatz_len[1] = 0;
+    collatz_len[2] = 1;
+    vector<long long> v {0, 1, 2};
     for(int a0=0;a0<t;a0++)
     {
         int length;
         cin >> length;
-        int dl;
-        int maks_lancuch=0;
-        int max_liczba=0;
-        for(int i=length;i>=1;i--)
-        {
-            dl=collatz(i);
-            if(dl>maks_lancuch)
-            {
-                maks_lancuch=dl;
-                max_liczba=i;
-            }
-        }
-        cout << max_liczba << endl;
+        cout << program(length, v) << endl;
     }
     return 0;
 }
