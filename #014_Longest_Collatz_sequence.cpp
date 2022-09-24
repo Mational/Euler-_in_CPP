074bb716b0b14b7edfad6953c249706eff3362e4
Mational <grzegorzmal40@gmail.com>
2022-09-25 00:30:33 +0200

    Update Euler014 - Work in progress

--- #014_Longest_Collatz_sequence.cpp
+++ #014_Longest_Collatz_sequence.cpp
@@ -4,29 +4,28 @@
 
 using namespace std;
 
-map<long long, int> collatz_len;
-
-int collatz(long long liczba)
+int collatz(long long liczba, map<int, unsigned short>&m)
 {
     if(liczba == 1) return 0;
 
-    if(collatz_len[liczba] == 0)
-        if(liczba%2 == 0)   collatz_len[liczba] = collatz(liczba/2) + 1;
-        else                collatz_len[liczba] = collatz(liczba*3+1) + 1;
+    if(m[liczba] == 0)
+        if(liczba%2 == 0)   m[liczba] = collatz(liczba/2, m) + 1;
+        else                m[liczba] = collatz((liczba*3+1)/2, m) + 2;
 
-    return collatz_len[liczba];
+    return m[liczba];
 }
 
-int program(int length, vector<long long>&v)
+int program(int n, vector<int>&v, map<int, unsigned short>&m)
 {
-    if(v.size() > length)   return v[length];
+    if(v.size() > n)   return v[n];
     int max_liczba = v[v.size()-1];
-    int maks_lancuch = collatz_len[max_liczba];
+    int maks_lancuch = m[max_liczba];
 
     int dl;
-    for(int i = v.size();i<=length;i++)
+    for(int i=v.size();i<=n;i++)
     {
-        dl=collatz(i);
+        dl=collatz(i, m);
+        m[i] = dl;
         if(dl>=maks_lancuch)
         {
             maks_lancuch=dl;
@@ -41,14 +40,15 @@ int main()
 {
     int t;
     cin >> t;
+    vector<int> v {0, 1, 2};
+    map<int, unsigned short> collatz_len;
     collatz_len[1] = 0;
     collatz_len[2] = 1;
-    vector<long long> v {0, 1, 2};
     for(int a0=0;a0<t;a0++)
     {
-        int length;
-        cin >> length;
-        cout << program(length, v) << endl;
+        int n;
+        cin >> n;
+        cout << program(n, v, collatz_len) << endl;
     }
     return 0;
 }
