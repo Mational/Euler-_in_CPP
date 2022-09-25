99bacffb8f78702b39f2182d95066f4b985afc85
Mational <grzegorzmal40@gmail.com>
2022-09-25 16:10:19 +0200

    Update Euler014 - działa również na HackerRank

--- #014_Longest_Collatz_sequence.cpp
+++ #014_Longest_Collatz_sequence.cpp
@@ -1,54 +1,63 @@
 #include <iostream>
-#include <map>
 #include <vector>
 
 using namespace std;
 
-int collatz(long long liczba, map<int, unsigned short>&m)
+void collatz(long long liczba, vector<int>&cl)
 {
-    if(liczba == 1) return 0;
-
-    if(m[liczba] == 0)
-        if(liczba%2 == 0)   m[liczba] = collatz(liczba/2, m) + 1;
-        else                m[liczba] = collatz((liczba*3+1)/2, m) + 2;
-
-    return m[liczba];
-}
-
-int program(int n, vector<int>&v, map<int, unsigned short>&m)
-{
-    if(v.size() > n)   return v[n];
-    int max_liczba = v[v.size()-1];
-    int maks_lancuch = m[max_liczba];
-
-    int dl;
-    for(int i=v.size();i<=n;i++)
+    long long current = liczba;
+    int chain = 0;
+    while(true)
     {
-        dl=collatz(i, m);
-        m[i] = dl;
-        if(dl>=maks_lancuch)
+        if(current<liczba)
         {
-            maks_lancuch=dl;
-            max_liczba=i;
+            cl[liczba] = chain + cl[current];
+            return;
         }
-        v.push_back(max_liczba);
+        if(current%2==0)
+        {
+            current/=2;
+            chain++;
+        }
+        else
+        {
+            current = (current*3+1)/2;
+            chain+=2;
+        }
     }
-    return max_liczba;
 }
 
 int main()
 {
     int t;
     cin >> t;
-    vector<int> v {0, 1, 2};
-    map<int, unsigned short> collatz_len;
-    collatz_len[1] = 0;
-    collatz_len[2] = 1;
+    int tab[t];
+    int m = 0;
     for(int a0=0;a0<t;a0++)
     {
-        int n;
-        cin >> n;
-        cout << program(n, v, collatz_len) << endl;
+        cin >> tab[a0];
+        if(tab[a0]>m) m=tab[a0];
     }
+
+    vector<int> chain_len(m+1);
+    vector<int> answer(m+1);
+
+    for(int i=2;i<=m;i++)
+        collatz(i, chain_len);
+
+    int curmax = 1;
+    int curindex = 1;
+    for(int i=2;i<=m;i++)
+    {
+        if(chain_len[i] >=curmax)
+        {
+            curmax = chain_len[i];
+            curindex = i;
+        }
+        answer[i] = curindex;
+    }
+
+    for(auto i:tab)
+        cout << answer[i] << endl;
     return 0;
 }
